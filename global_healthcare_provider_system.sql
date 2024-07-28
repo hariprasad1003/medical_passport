9ff8d833f79e272b200bd5b222685220dafafebb
hariprasad1003 <hariprasadgurunathan10@gmail.com>
2024-07-28 22:02:01 +0100

    database script added

---
+++ global_healthcare_provider_system.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE global_healthcare_provider_system;
+USE global_healthcare_provider_system;
+
+CREATE TABLE healthcare_provider (
+    healthcare_provider_id INT PRIMARY KEY,
+    healthcare_provider_name VARCHAR(100) NOT NULL,
+    healthcare_provider_domain VARCHAR(100) NOT NULL,
+    house_number_post_box_number VARCHAR(50),
+    post_code VARCHAR(10),
+    country VARCHAR(50)
+);
